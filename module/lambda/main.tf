71b11ed85b2ece4e33b7e68eb0de9b90cde0d64c
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 19:20:31 -0600

    Create main.tf

---
+++ module/lambda/main.tf
@@ -0,0 +1,28 @@
+resource "aws_lambda_function" "form_handler" {
+  function_name = var.function_name
+  runtime       = "nodejs14.x"
+  handler       = "index.handler"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.function_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
