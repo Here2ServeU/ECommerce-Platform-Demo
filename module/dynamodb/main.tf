fac341f82352a3b32fa2249adad1da12d499492d
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 19:19:07 -0600

    Update main.tf

--- module/dynamodb/main.tf
+++ module/dynamodb/main.tf
@@ -1,21 +1,9 @@
-resource "aws_dynamodb_table" "this" {
+resource "aws_dynamodb_table" "form_submissions" {
   name           = var.table_name
-  billing_mode   = var.billing_mode
-  hash_key       = var.hash_key
-  range_key      = var.range_key
-
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
   attribute {
-    name = var.hash_key
-    type = var.hash_key_type
+    name = "id"
+    type = "S"
   }
-
-  dynamic "attribute" {
-    for_each = var.range_key != null ? [var.range_key] : []
-    content {
-      name = var.range_key
-      type = var.range_key_type
-    }
-  }
-
-  tags = var.tags
 }
