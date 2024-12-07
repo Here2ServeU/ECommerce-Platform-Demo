476d83d4b825eb2473a865e9e323dc8acbca3caf
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 19:19:30 -0600

    Update variables.tf

--- module/dynamodb/variables.tf
+++ module/dynamodb/variables.tf
@@ -1,38 +1,4 @@
 variable "table_name" {
-  description = "Name of the DynamoDB table"
+  description = "The name of the DynamoDB table"
   type        = string
-}
-
-variable "billing_mode" {
-  description = "Billing mode for DynamoDB (PROVISIONED or PAY_PER_REQUEST)"
-  type        = string
-  default     = "PAY_PER_REQUEST"
-}
-
-variable "hash_key" {
-  description = "Primary key hash attribute name"
-  type        = string
-}
-
-variable "hash_key_type" {
-  description = "Primary key hash attribute type (S or N)"
-  type        = string
-}
-
-variable "range_key" {
-  description = "Primary key range attribute name (optional)"
-  type        = string
-  default     = null
-}
-
-variable "range_key_type" {
-  description = "Primary key range attribute type (S or N), required if range_key is set"
-  type        = string
-  default     = null
-}
-
-variable "tags" {
-  description = "Tags to apply to the table"
-  type        = map(string)
-  default     = {}
 }
