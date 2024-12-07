db21a0b52d84fb9f0244c1d7ce60dbeb967e021f
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 19:23:55 -0600

    Update outputs.tf

--- module/api_gateway/outputs.tf
+++ module/api_gateway/outputs.tf
@@ -1,4 +1,14 @@
+output "api_id" {
+  description = "The ID of the API Gateway"
+  value       = aws_apigatewayv2_api.api.id
+}
+
 output "api_endpoint" {
-  description = "Endpoint URL of the API Gateway"
-  value       = aws_apigatewayv2_stage.this.invoke_url
+  description = "The URL of the API Gateway"
+  value       = aws_apigatewayv2_stage.stage.invoke_url
+}
+
+output "integration_id" {
+  description = "The ID of the API Gateway Lambda integration"
+  value       = aws_apigatewayv2_integration.lambda_integration.id
 }
