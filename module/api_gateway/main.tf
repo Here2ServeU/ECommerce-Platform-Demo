cfa56fc6d8827c172b40b51b94d5aa053c741f54
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 19:22:07 -0600

    Update main.tf

--- module/api_gateway/main.tf
+++ module/api_gateway/main.tf
@@ -1,14 +1,10 @@
-resource "aws_apigatewayv2_api" "this" {
+resource "aws_apigatewayv2_api" "api" {
   name          = var.api_name
   protocol_type = "HTTP"
-
-  tags = var.tags
 }
 
-resource "aws_apigatewayv2_stage" "this" {
-  api_id      = aws_apigatewayv2_api.this.id
-  name        = var.stage_name
-  auto_deploy = true
-
-  tags = var.tags
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.form_handler.invoke_arn
 }
