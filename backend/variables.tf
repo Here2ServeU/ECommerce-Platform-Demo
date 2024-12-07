6526eda5d737f10acce4949ca8dd14788abce63a
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-06 18:03:50 -0600

    Update variables.tf

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  description = "Name of the Region where we want to deploy"
+  type = string
+  default = "us-east-1" 
+
 variable "backend_name" {
   description = "Name of the S3 Bucket to store the State File"
   type = string
